6d7393cd52ea243524e1dbcdfd5d7d3cc125cf58
kurian kurian <kurianck.mail@gmail.com>
2021-04-10 08:10:10 +0530

    recover middle ware

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"eqregister/api"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Recover())
 	e.GET("/question/:id", api.GetQuestionByIDHandler)
 	e.PUT("/question/:id", api.UpdateQuestionByIDHandler)
 	e.POST("/question", api.InsertQuestionHandler)
